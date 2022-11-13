e48c06d8c34cf1f67d118867e8f2d2b739140c8b
Saveliy <saveliy.kan@gmail.com>
2022-11-13 22:55:05 +0300

    Сommit 1

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,68 @@
-public class Main {
-    public static void main(String[] args) {
 
-        System.out.println("pull Requests");
+public class Main {
+
+    int[] arr = generateRandomArray();
+
+    public static int[] generateRandomArray()
+    {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++)
+        {
+            arr[i] = random.nextInt(50_000) + 50_000;
+        }
+        return arr;
     }
 
+    public static void main(String[] args) {
+
+        System.out.println("Задача 1");
+
+        int sumSalary=0;
+        int[] arr = generateRandomArray();
+        for (int i = 0; i < arr.length; i++)
+        {
+            sumSalary +=arr[i];
+        }
+
+        System.out.println("Сумма трат за месяц составила " +  sumSalary + " рублей");
+
+        System.out.println("задача 2");
+
+        int minSum=arr[0];
+        int maxSum=0;
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (arr[i]> maxSum)
+            {
+                maxSum = arr [i];
+            }
+            if (minSum > arr [i])
+             {
+                minSum = arr [i];
+             }
+        }
+        System.out.println("Максимальная сумма трат за день составляет: " + maxSum + " рублей");
+        System.out.println("Минимальная сумма трат за день составляет: " + minSum + " рублей");
+
+        System.out.println("Задача 3");
+
+        int averageCosts = sumSalary / 30;
+        System.out.println("Средняя сумма трат за месяц составила " + averageCosts + " рублей в день");
+
+        System.out.println("Задача 4");
+
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1 ;i >= 0; i--){
+            System.out.print(reverseFullName[i]);
+        }
 
 
-    //тест
-//тест
+
+
+
+
+
+    }
 
 }
