0255fffa9322e5a6695d0ee9184ab30bd8db6481
Saveliy <saveliy.kan@gmail.com>
2022-11-13 23:00:20 +0300

    Сommit 1

--- src/Main.java
+++ src/Main.java
@@ -58,7 +58,6 @@ public class Main {
 
         System.out.println("Задача 4");
 
-
         char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         for (int i = reverseFullName.length - 1 ;i >= 0; i--){
             System.out.print(reverseFullName[i]);
